7fff4b0778689c979c266202992c420994c93163
Lucas <luc1497@gmail.com>
2023-09-24 20:14:46 -0300

    Foi Adicionado o arquivo que conecta ao banco

---
+++ conectaBanco.php
@@ -0,0 +1,10 @@
+<?php
+$servidor = "containers-us-west-149.railway.app";
+$userbd = "root";
+$porta = "6153";
+$senhabd = "jGDl6kTLQ2RLNKdzuxsh";
+$bancodedados = "railway";
+
+$conectar = new mysqli($servidor, $userbd, $senhabd, $bancodedados, $porta);
+
+?>
