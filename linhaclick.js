0d48fc91d29ecc79eda635ac5361568d33e65f23
Lucas <luc1497@gmail.com>
2023-10-05 22:47:03 -0300

    correção de bug no botão excluir chamado

--- linhaclick.js
+++ linhaclick.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     const linhas = document.querySelectorAll('.linha');
 
     linhas.forEach(linha =>{
-        linha.addEventListener("click", function(event){
-            event.preventDefault();
+        linha.addEventListener("click", function(){
             const linhavalor = this.getAttribute("value");
             console.log(linhavalor);
             botaoeditar = document.getElementById("editar" + linhavalor);
