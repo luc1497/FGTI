ead31b0a4c1c4dc5341bd23ac941889654ffe824
Lucas <luc1497@gmail.com>
2023-10-13 19:55:32 -0300

    efetuada a troca de banco de dados

--- conectaBanco.php
+++ conectaBanco.php
@@ -1,9 +1,9 @@
 <?php
-$servidor = "containers-us-west-149.railway.app";
-$userbd = "root";
-$porta = "6153";
-$senhabd = "jGDl6kTLQ2RLNKdzuxsh";
-$bancodedados = "railway";
+$servidor = "srv883.hstgr.io";
+$userbd = "u565999661_root";
+$porta = "3306";
+$senhabd = "Leiloes3000pb*";
+$bancodedados = "u565999661_xaropin";
 
 $conectar = new mysqli($servidor, $userbd, $senhabd, $bancodedados, $porta);
 
