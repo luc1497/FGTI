87c2a77aa9eb76a32ae878ca0c3fac39e593390d
Lucas <luc1497@gmail.com>
2023-10-15 20:54:16 -0300

    adição do arquivo que conecta ao banco de dados

---
+++ conectaBanco.php
@@ -0,0 +1,10 @@
+<?php
+
+$bdhost = "srv883.hstgr.io";
+$loginbd = "u565999661_root";
+$senhabd = "Leiloes3000pb*";
+$bdname = "u565999661_xaropin";
+
+$conectar = new mysqli($bdhost, $loginbd, $senhabd, $bdname);
+
+?>
