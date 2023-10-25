55bee1b14e430b428f5c18122a77d1c0cbef7063
unknown <luc1497@gmail.com>
2023-10-25 11:52:40 -0300

    mudança no ícone do módulo de estoque

--- appHome.php
+++ appHome.php
@@ -47,9 +47,9 @@ if (!isset($_SESSION['id'])){
         
         <div class="card">
             <a href="estoqueHome.php">
-                <svg xmlns="http://www.w3.org/2000/svg" width="50%"  fill="currentColor" class="bi bi-file-earmark" viewBox="0 0 16 16">
-                    <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/>
-                </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="50%" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16">
+                <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
+            </svg>
 
                 <h1>GESTÃO DE ESTOQUE</h1>
             </a>
